1395537febfb2675533c0b52b239cf388d5db5a3
Chen Yonati <chen.yonati@gmail.com>
2021-04-12 10:54:08 +0300

    Show user name

--- src/authenticated-app.js
+++ src/authenticated-app.js
@@ -1,11 +1,9 @@
 import React from "react";
 import {Navbar, Nav} from 'react-bootstrap'
 
-
 function AuthenticatedApp({user}) {
     return (
       <>
-      <span>User {user}</span>
       <Navbar bg="dark" variant="dark">
         <Navbar.Brand href="#home">Navbar</Navbar.Brand>
         <Nav className="mr-auto">
@@ -13,6 +11,7 @@ function AuthenticatedApp({user}) {
           <Nav.Link href="#features">Features</Nav.Link>
           <Nav.Link href="#pricing">Pricing</Nav.Link>
         </Nav>
+        <span className="mr-sm-2 text-white">Hello <b>{user}</b></span>
       </Navbar>
     </>
     );
