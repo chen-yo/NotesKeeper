693f471cc56ebd45d9dd15609b9cfe15ed974126
Chen Yonati <chen.yonati@gmail.com>
2021-04-18 10:25:56 +0300

    Add auto-login by user id - kind of like token

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import * as React from 'react'
-import * as auth from './auth-provider'
-import {BrowserRouter as Router} from 'react-router-dom'
-import {client} from './utils/api-client'
-import {useAsync} from './utils/hooks'
-import {AuthenticatedApp} from './authenticated-app'
-import {UnauthenticatedApp} from './unauthenticated-app'
-import {useAppContext} from './appdata'
+import * as React from "react";
+import * as auth from "./auth-provider";
+import { BrowserRouter as Router } from "react-router-dom";
+import { client } from './utils/api-client'
+import { useAsync } from "./utils/hooks";
+import { AuthenticatedApp } from "./authenticated-app";
+import { UnauthenticatedApp } from "./unauthenticated-app";
+import { useAppContext } from "./appdata";
 
 async function getUser() {
+  const token = await auth.getToken();
+  let user = null;
 
-  
-  let user = null
-
-  const token = await auth.getToken()
   if (token) {
-    const data = {user: 'chen'}
-    user = data.user
+    user = await client("user/me", { headers: { ID: token } });
   }
 
-  return null;
-  
+  return user;
 }
 
 function App() {
-  const [state, dispatch] = useAppContext()
+  const [state, dispatch] = useAppContext();
 
   const {
     data: user,
@@ -34,21 +30,24 @@ function App() {
     isSuccess,
     run,
     setData,
-  } = useAsync()
+  } = useAsync();
 
   React.useEffect(() => {
-    run(getUser()).then(data=>dispatch({type: 'LOGIN', user: data})).catch()
-  }, [run, dispatch])
+    run(getUser())
+      .then((data) => dispatch({ type: "LOGIN", user: data }))
+      .catch();
+  }, [run, dispatch]);
 
-  const login = form => auth.login(form).then(user => dispatch({type: 'LOGIN', user: user}))
-  const register = form => auth.register(form).then(user => setData(user))
+  const login = (form) =>
+    auth.login(form).then((user) => dispatch({ type: "LOGIN", user: user }));
+  const register = (form) => auth.register(form).then((user) => setData(user));
   const logout = () => {
-    auth.logout()
-    setData(null)
-  }
+    auth.logout();
+    setData(null);
+  };
 
   if (isLoading || isIdle) {
-    return <span>Loading...</span>
+    return <span>Loading...</span>;
   }
 
   if (isError) {
@@ -57,21 +56,21 @@ function App() {
         <p>Uh oh... There's a problem. Try refreshing the app.</p>
         <pre>{error.message}</pre>
       </div>
-    )
+    );
   }
 
   if (isSuccess) {
-    const {user} = state
-    console.log('isSucc', user)
-    const props = {login, register, logout}
+    const { user } = state;
+    console.log("isSucc", user);
+    const props = { login, register, logout };
     return user ? (
       <Router>
         <AuthenticatedApp {...props} />
       </Router>
     ) : (
       <UnauthenticatedApp {...props} />
-    )
+    );
   }
 }
 
-export {App}
+export { App };
--- src/auth-provider.js
+++ src/auth-provider.js
@@ -6,12 +6,13 @@ async function getToken() {
   // if we were a real auth provider, this is where we would make a request
   // to retrieve the user's token. (It's a bit more complicated than that...
   // but you're probably not an auth provider so you don't need to worry about it).
-  return window.localStorage.getItem(localStorageKey)
+  let id = window.localStorage.getItem(localStorageKey)
+  return id
 }
 
 function handleUserResponse(user) {
-  const {email} = user
-  window.localStorage.setItem(localStorageKey, email)
+  const {id} = user
+  window.localStorage.setItem(localStorageKey, id)
   return user
 }
 
